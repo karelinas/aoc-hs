8bdc8b78639093f20d61ed6bdc0d393310f9d33d
karelinas <45606302+karelinas@users.noreply.github.com>
2022-07-19 15:52:56 +0300

    Day 02: short circuit evenDivision()
    Previously we kept looking for more mutually divisible values even after
    we had found one. Now we stop looking after we have found the first one,
    saving some work.

--- 2017/day02.hs
+++ 2017/day02.hs
@@ -16,13 +16,14 @@ evenDivisionValue (a:b:xs) = if (max a b) `mod` (min a b) == 0
                              then (max a b) `div` (min a b)
                              else 0
 
+firstNonZero :: [Int] -> Int
+firstNonZero (x:xs) = if x /= 0 then x else firstNonZero xs
+
 evenDivision :: [Int] -> Int
-evenDivision line = sum (map evenDivisionValue (combinations 2 line))
+evenDivision line = firstNonZero (map evenDivisionValue (combinations 2 line))
 
 checksum :: ([Int] -> Int) -> [[Int]] -> Int
 checksum lineValuator spreadsheet = sum(map lineValuator spreadsheet)
-
-
 
 main :: IO ()
 main = do
