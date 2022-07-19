2bfcdd5b390f54119c8e58203125b7e56ecda1ec
karelinas <45606302+karelinas@users.noreply.github.com>
2022-07-19 21:15:09 +0300

    Day 02: Refactor with newly learned Haskell
    Making use of some newly learned features, such as:
    
    * The `Maybe` type
    * `case` keyword for checking if the `Maybe` type has a value
    * Similarly to a previous refactor of day01, the `.` operator, `$`
      operator, `where`, etc.
    
    Also improved naming a bit (I think).

--- 2017/day02.hs
+++ 2017/day02.hs
@@ -4,23 +4,29 @@ import Data.List
 intWords :: String -> [Int]
 intWords s = map read (words s)
 
-differenceOfExtremes :: [Int] -> Int
-differenceOfExtremes line = maximum(line) - minimum(line)
-
 -- Borrowed from https://wiki.haskell.org/99_questions/Solutions/26
 combinations :: Int -> [a] -> [[a]]
 combinations k ns = filter ((k==).length) (subsequences ns)
 
-evenDivisionValue :: [Int] -> Int
-evenDivisionValue (a:b:xs) = if (max a b) `mod` (min a b) == 0
-                             then (max a b) `div` (min a b)
-                             else 0
+firstValue :: [Maybe Int] -> Int
+firstValue (x:xs) =
+    case x of
+        Just n -> n
+        Nothing -> firstValue xs
 
-firstNonZero :: [Int] -> Int
-firstNonZero (x:xs) = if x /= 0 then x else firstNonZero xs
+evenDivisionValue :: [Int] -> Maybe Int
+evenDivisionValue (a:b:_)
+    | (bigger `mod` smaller) == 0 = Just (bigger `div` smaller)
+    | otherwise = Nothing
+    where
+        bigger  = max a b
+        smaller = min a b
 
-evenDivision :: [Int] -> Int
-evenDivision line = firstNonZero (map evenDivisionValue (combinations 2 line))
+differenceValuator :: [Int] -> Int
+differenceValuator line = maximum(line) - minimum(line)
+
+divisibilityValuator :: [Int] -> Int
+divisibilityValuator = firstValue . (map evenDivisionValue) . (combinations 2)
 
 checksum :: ([Int] -> Int) -> [[Int]] -> Int
 checksum lineValuator spreadsheet = sum(map lineValuator spreadsheet)
@@ -29,5 +35,5 @@ main :: IO ()
 main = do
     input <- getContents
     let spreadsheet = map intWords (lines input)
-    putStrLn (show (checksum differenceOfExtremes spreadsheet))
-    putStrLn (show (checksum evenDivision spreadsheet))
+    putStrLn $ show $ checksum differenceValuator spreadsheet
+    putStrLn $ show $ checksum divisibilityValuator spreadsheet
