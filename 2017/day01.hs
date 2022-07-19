ebd050254ad8ef13412489fee88b821a4ce771fa
karelinas <45606302+karelinas@users.noreply.github.com>
2022-07-19 20:42:21 +0300

    Day 01: use newly learned features
    Incorporate some newly learned Haskell features, such as:
    
    * The `$` sign to clean up some parentheses from prints
    * Function composition with the `.` operator for the solver, again to
      clean up parentheses
    * The `where` keyword

--- 2017/day01.hs
+++ 2017/day01.hs
@@ -1,29 +1,23 @@
 import Data.Char
 
-rotate :: Int -> [a] -> [a]
-rotate _ [] = []
-rotate n (x:xs) = if n <= 0 || null xs
-                  then (x:xs)
-                  else rotate (n - 1) (xs ++ [x])
-
 pairs :: Int -> [a] -> [(a, a)]
-pairs n xs = zip xs (rotate n xs)
+pairs offset xs = zip xs xsRotated
+    where xsRotated = (drop offset xs) ++ xs
 
-pairSum :: (Int, Int) -> Int
-pairSum (a, b) = if a == b then a else 0
+pairValue :: (Int, Int) -> Int
+pairValue (a, b)
+    | (a == b) = a
+    | otherwise = 0
 
 halfpoint :: [a] -> Int
 halfpoint xs = (length xs) `div` 2
 
-partOneSolution :: [Int] -> Int
-partOneSolution digits = sum (map pairSum (pairs 1 digits))
-
-partTwoSolution :: [Int] -> Int
-partTwoSolution digits = sum (map pairSum (pairs (halfpoint digits) digits))
+solution :: Int -> ([Int] -> Int)
+solution offset = sum . (map pairValue) . (pairs offset)
 
 main :: IO ()
 main = do
     input <- getLine
-    let digits = map digitToInt input
-    putStrLn (show (partOneSolution digits))
-    putStrLn (show (partTwoSolution digits))
+    let captcha = map digitToInt input
+    putStrLn $ show $ solution 1 captcha
+    putStrLn $ show $ solution (halfpoint captcha) captcha
